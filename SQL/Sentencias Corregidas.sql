0c7f64c5f198a323e0d98646e7321530671546ce
Alejandro Cubillos <32422532+AlejoCuPe@users.noreply.github.com>
2017-11-13 22:48:38 -0500

    Correcciones Menores

--- SQL/Sentencias Corregidas.sql
+++ SQL/Sentencias Corregidas.sql
@@ -152,7 +152,7 @@ COMMIT;
 /* Insert producto */
 INSERT INTO producto (Nombre, Precio, Marca, IDLocal, Descripcion, NombreImagen, Stock) VALUES
 	("Samsung Galaxy Note 7", 2000000, "Samsung", 4, "Explota.","Samsung-Samsung Galaxy Note 7.png", 10),
-	("Consola PS4 Pro", 1400000, "Sony", 1, "Consola Videojuegos.","Sony-Consola PS$ Pro.png", 3),
+	("Consola PS4 Pro", 1400000, "Sony", 1, "Consola Videojuegos.","Sony-Consola PS4 Pro.png", 3),
 	("Televisor X94E", 4000000, "Sony", 1, "Televisión 4K.","Sony-Televisor X94E.png", 0),
 	("Consola Xbox One X", 1800000, "Microsoft", 2, "Consola Videojuegos.","Microsoft-Consola Xbox One X.png", 1000),
 	("Office 365", 200000, "Microsoft", 2, "Licencia Office.","Microsoft-Office 365.png", 10),
